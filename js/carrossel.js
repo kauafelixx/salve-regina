e5c0e56b43d0073b31044dd9c46cdd7335c020af
Kauã Felix <161839119+kauafelixx@users.noreply.github.com>
2025-07-15 17:31:47 -0300

    feat: adiciona Mistérios do Terço e melhorias no design
    - Adicionados os Mistérios Gozosos, Dolorosos e Gloriosos
    - Novo design de interface com atualização nos botões e fundo
    - Alteração na paleta de cores do projeto
    - Inclusão de carrossel de imagens para contemplação dos mistérios

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,34 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Seleciona todos os elementos com a classe 'carrossel-placeholder'
-    const carouselPlaceholders = document.querySelectorAll('.carrossel-placeholder');
+document.addEventListener('DOMContentLoaded', function () {
+  const carouselPlaceholders = document.querySelectorAll('.carrossel-placeholder');
 
-    // Itera sobre cada placeholder encontrado para criar um carrossel independente
-    carouselPlaceholders.forEach(placeholder => {
-        // Seleciona todas as imagens dentro do carrossel atual
-        const carouselImages = placeholder.querySelectorAll('.carousel-slides img');
-        let currentIndex = 0; // Índice da imagem atualmente visível
-        const intervalTime = 5000; // Tempo em milissegundos para a troca (3 segundos)
+  carouselPlaceholders.forEach(placeholder => {
+    const imageWrappers = placeholder.querySelectorAll('.img-wrapper');
+    let currentIndex = 0;
+    const intervalTime = 5000;
 
-        // Se houver imagens, exibe a primeira imagem ao carregar a página
-        if (carouselImages.length > 0) {
-            carouselImages[currentIndex].classList.add('active');
-        }
+    if (imageWrappers.length > 0) {
+      imageWrappers[currentIndex].classList.add('active');
+    }
 
-        // Função para mostrar a próxima imagem
-        function showNextImage() {
-            // Remove a classe 'active' da imagem atual, escondendo-a
-            carouselImages[currentIndex].classList.remove('active');
+    function showNextImage() {
+      imageWrappers[currentIndex].classList.remove('active');
+      currentIndex = (currentIndex + 1) % imageWrappers.length;
+      imageWrappers[currentIndex].classList.add('active');
+    }
 
-            // Calcula o índice da próxima imagem (loop infinito)
-            currentIndex = (currentIndex + 1) % carouselImages.length;
-
-            // Adiciona a classe 'active' à próxima imagem, mostrando-a
-            carouselImages[currentIndex].classList.add('active');
-        }
-
-        // Inicia o autoplay se houver mais de uma imagem para alternar
-        if (carouselImages.length > 1) {
-            setInterval(showNextImage, intervalTime);
-        }
-    });
+    if (imageWrappers.length > 1) {
+      setInterval(showNextImage, intervalTime);
+    }
+  });
 });
