741ca538289972114883b42a2e90ccf16bd9a107
Kauã Felix <161839119+kauafelixx@users.noreply.github.com>
2025-07-17 21:25:04 -0300

    feat: inicia criação da aba da Via Sacra com imagens e ajustes iniciais
    - Iniciada a estrutura da seção da Via Sacra
    - Adicionadas imagens iniciais das estações
    - Realizados os primeiros ajustes no layout e estrutura da página

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.getElementById("botaoTerco").addEventListener('click', () => {
     window.location.href = 'pages/misterios.html';
-})
+});
+
+document.getElementById("botaoViaSacra").addEventListener('click', () => {
+    window.location.href = 'pages/viasacra.html';
+});
