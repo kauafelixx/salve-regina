28cdf5f199674ec6ac4303618bf192e5408b8d7a
Kauã Felix <161839119+kauafelixx@users.noreply.github.com>
2025-07-28 19:03:10 -0300

    refactor: ajusta layout dos botões e remove tentativa da página de Orações Marianas
    - Atualizado o layout dos botões para melhorar a interface
    - Iniciada a criação da página de Orações Marianas, mas removida devido a erros
    - Página será refeita do zero em outro momento

--- js/oracoes.js
+++ js/oracoes.js
@@ -1,3 +1,7 @@
 document.getElementById("oracoesFundamentais").addEventListener('click', () => {
     window.location.href = 'oracoesfundamentais.html';
 });
+
+document.getElementById("oracoesMarianas").addEventListener('click', () => {
+    window.location.href = 'oracoesmarianas.html';
+});
